222740d299a4b00807b7e39cb73fc1ca94e9cb81
quincr <quincrafter@gmail.com>
2024-11-05 20:28:27 -0600

    pressable element, element base class, ticking, management

--- src/gui.py
+++ src/gui.py
@@ -1,6 +1,51 @@
 from __future__ import annotations
 
 import pygame as pg
+from typing import Any, Callable
+
+class Manager():
+    def __init__(self: Manager) -> None:
+        self.elements: list[BaseElement] = []
+
+    def AddElement(self: Manager, element: type[BaseElement]) -> None:
+        self.elements.append(element)
+    
+    def Tick(self: Manager) -> None:
+        is_pressing = pg.mouse.get_pressed(3)[0]
+        just_pressed = pg.mouse.get_just_pressed()[0]
+
+        for element in self.elements:
+            element.is_hovering = element.IsMouseHovering()
+            element.is_pressed = is_pressing and element.is_hovering
+            element.is_just_pressed = just_pressed and element.is_hovering
+
+            element.Tick()
+
+    def Render(self: Manager) -> None:
+        window_surface = pg.display.get_surface()
+
+        for element in self.elements:
+            element.Render(window_surface)
+
+class BaseElement():
+    is_hovering: bool = False
+    is_pressed: bool = False
+    is_just_pressed: bool = False
+
+    def __init__(self: BaseElement) -> None:
+        pass
+
+    def Update(self: BaseElement) -> None:
+        return
+    
+    def IsMouseHovering(self: BaseElement) -> bool:
+        return False
+    
+    def Tick(self: BaseElement) -> None:
+        pass
+    
+    def Render(self: BaseElement, surface: pg.Surface) -> None:
+        pass
 
 class Font():
     def __init__(self: Font, path: str) -> None:
@@ -13,7 +58,7 @@ class Font():
         
         return self._font_register[size]
 
-class Text():
+class Text(BaseElement):
     def __init__(self: Text, font: Font, text: str, size: int, color: pg.Color, position: pg.Vector2) -> None:
         self.position = position
         self.size = size
@@ -29,15 +74,43 @@ class Text():
     def Render(self: Text, surface: pg.Surface) -> None:
         surface.blit(self._renderred, self.position)
 
-
-class Box():
+class Box(BaseElement):
     def __init__(self: Box, position: pg.Vector2, color: pg.Color, size: pg.Vector2) -> None:
         self.position = position
         self.color = color
         self.size = size
-    
+
     def IsMouseHovering(self: Box) -> bool:
         return pg.Rect(self.position, self.size).collidepoint(*pg.mouse.get_pos())
 
     def Render(self: Box, surface: pg.Surface) -> None:
         pg.draw.rect(surface, self.color, (self.position, self.size))
+
+class Pressable(BaseElement):
+    def __init__(self: Pressable, position: pg.Vector2, size: pg.Vector2, passive_color: pg.Color, hover_color: pg.Color, pressed_color: pg.Color, event: Callable, event_args: tuple[Any] = ()) -> None:
+        self.position = position
+        self.size = size
+
+        self.passive_color = passive_color
+        self.hover_color = hover_color
+        self.pressed_color = pressed_color
+
+        self.event = event
+        self.event_args = event_args
+    
+    def IsMouseHovering(self: Pressable) -> bool:
+        return pg.Rect(self.position, self.size).collidepoint(*pg.mouse.get_pos())
+
+    def Tick(self: Pressable) -> None:
+        if self.is_just_pressed:
+            self.event(self, *self.event_args)
+
+    def Render(self: Pressable, surface: pg.Surface) -> None:
+        _color = self.passive_color
+        if self.is_hovering:
+            if self.is_pressed:
+                _color = self.pressed_color
+            else:
+                _color = self.hover_color
+
+        pg.draw.rect(surface, _color, (self.position, self.size))
--- src/main.py
+++ src/main.py
@@ -2,13 +2,23 @@ import pygame as pg
 
 pg.init()
 
-from gui import Font, Text, Box
+from gui import Manager, Font, Text, Box, Pressable
 
 display = pg.display.set_mode((800, 600))
+
+manager = Manager()
+
+def TestPressEvent(pressable: Pressable, test_arg_1: int, test_arg_2: str) -> None:
+    print("Pressable field has been pressed.", test_arg_1, test_arg_2)
 
 font = Font('./assets/fonts/Garet-Book.ttf')
 text = Text(font, 'This is test text.', 24, (255, 255, 255), pg.Vector2(5, 5))
 box = Box((50, 50), (255, 0, 127), (50, 50))
+pressable = Pressable((50, 200), (50, 25), (128, 0, 0), (0, 128, 0), (0, 0, 128), TestPressEvent, (1, 'test_string_arg'))
+
+manager.AddElement(text)
+manager.AddElement(box)
+manager.AddElement(pressable)
 
 running = True
 while running:
@@ -16,10 +26,11 @@ while running:
         if ev.type == pg.QUIT:
             running = False
     
-    text.Render(display)
+    manager.Tick()
+    manager.Render()
 
-    if not box.IsMouseHovering():
-        box.Render(display)
+    #if not box.IsMouseHovering():
+    #    box.Render(display)
 
     pg.display.flip()
     display.fill(0)
